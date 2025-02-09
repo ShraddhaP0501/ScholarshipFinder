940aef64828eff0a6ca67385117113d766fc4b58
Shraddha <shraddhap0501@gmail.com>
2025-02-09 12:54:45 +0530

    Category Wise Button Working

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def home():
     return render_template("home.html")
 
 
-@app.route("/find", methods=["POST"])
+@app.route("/find", methods=["POST", "GET"])
 def findscholarship():
     try:
         category = request.form.get("category")
@@ -61,6 +61,115 @@ def allscholarships():
     except Exception as e:
         print("Error fetching all scholarships:", e)
         return "Error: Unable to retrieve data."
+
+    # Route to display OBC scholarships
+
+
+@app.route("/general-scholarships")
+def general_scholarships():
+    try:
+        conn = getdatabase()
+        cursor = conn.cursor()
+        cursor.execute(
+            "SELECT DISTINCT ScholarshipName, Details, Eligibility FROM scholarships WHERE category = 'general'"
+        )
+        scholarships = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        return render_template("general_scholarship.html", scholarships=scholarships)
+    except Exception as e:
+        print(e)
+        return "Error fetching General scholarships"
+
+
+# Route to display SEBC scholarships
+@app.route("/scbc-scholarships")
+def scbc_scholarships():
+    try:
+        conn = getdatabase()
+        cursor = conn.cursor()
+        cursor.execute(
+            "SELECT DISTINCT ScholarshipName, Details, Eligibility FROM scholarships WHERE category = 'scbc'"
+        )
+        scholarships = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        return render_template("scbc_scholarship.html", scholarships=scholarships)
+    except Exception as e:
+        print(e)
+        return "Error fetching SEBC scholarships"
+
+
+# Route to display OBC scholarships
+@app.route("/obc-scholarships")
+def obc_scholarships():
+    try:
+        conn = getdatabase()
+        cursor = conn.cursor()
+        cursor.execute(
+            "SELECT DISTINCT ScholarshipName, Details, Eligibility FROM scholarships WHERE category = 'OBC'"
+        )
+        scholarships = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        return render_template("obc_scholarship.html", scholarships=scholarships)
+    except Exception as e:
+        print(e)
+        return "Error fetching OBC scholarships"
+
+
+# Route to display ST scholarships
+@app.route("/st-scholarships")
+def st_scholarships():
+    try:
+        conn = getdatabase()
+        cursor = conn.cursor()
+        cursor.execute(
+            "SELECT DISTINCT ScholarshipName, Details, Eligibility FROM scholarships WHERE category = 'st'"
+        )
+        scholarships = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        return render_template("st_scholarship.html", scholarships=scholarships)
+    except Exception as e:
+        print(e)
+        return "Error fetching ST scholarships"
+
+
+# Route to display SC scholarships
+@app.route("/sc-scholarships")
+def sc_scholarships():
+    try:
+        conn = getdatabase()
+        cursor = conn.cursor()
+        cursor.execute(
+            "SELECT  DISTINCT ScholarshipName, Details, Eligibility FROM scholarships WHERE category = 'sc'"
+        )
+        scholarships = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        return render_template("sc_scholarship.html", scholarships=scholarships)
+    except Exception as e:
+        print(e)
+        return "Error fetching SC scholarships"
+
+
+# Route to display EWS scholarships
+@app.route("/ews-scholarships")
+def ews_scholarships():
+    try:
+        conn = getdatabase()
+        cursor = conn.cursor()
+        cursor.execute(
+            "SELECT DISTINCT ScholarshipName, Details, Eligibility FROM scholarships WHERE category = 'ews'"
+        )
+        scholarships = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        return render_template("ews_scholarship.html", scholarships=scholarships)
+    except Exception as e:
+        print(e)
+        return "Error fetching EWS scholarships"
 
 
 @app.errorhandler(404)
